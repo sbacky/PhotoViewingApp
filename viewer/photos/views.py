ed5b1c86889538ccb14691800749dd3b45b80e22
Neil <nhc.crum@gmail.com>
2023-03-11 09:58:34 -0700

    Add support to change photo display
    urls.py - url to send post requests to
    views.py - process post request and return correct template
    *.html - html templates

--- viewer/photos/views.py
+++ viewer/photos/views.py
@@ -5,17 +5,60 @@ import logging
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.utils import timezone
+from django.views.decorators.http import require_POST
 
 from photos.models import Photo
 
 # Get instance of logger using namespace
 logger = logging.getLogger(__name__)
 
+
 def dashboard(request: HttpRequest) -> HttpResponse:
+    display = "list"
+    photos = Photo.objects.all().filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')
     return render(
         request,
-        "photos/dashboard.html"
+        "photos/dashboard.html",
+        {
+            "display": display,
+            "photos": photos
+        }
     )
+
+
+@require_POST
+def photo_list(request: HttpRequest) -> HttpResponse:
+    display = "list"
+    photos = Photo.objects.all().filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')
+    return render(
+        request,
+        "photos/photo_display.html",
+        {
+            "display": display,
+            "photos": photos
+        }
+    )
+
+
+@require_POST
+def photo_columns(request: HttpRequest) -> HttpResponse:
+    display = "column"
+    photos = Photo.objects.all().filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')
+    return render(
+        request,
+        "photos/photo_display.html",
+        {
+            "display": display,
+            "photos": photos
+        }
+    )
+
 
 def detail(request: HttpResponse, pk: int) -> HttpResponse:
     pass
