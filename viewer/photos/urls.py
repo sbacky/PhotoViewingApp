7905cbb0ed835e4df7febcd2e3f524fb40428a02
Neil <nhc.crum@gmail.com>
2023-03-12 10:47:24 -0700

    Update url.py, views.py and photo_list.html
    Add pagination for photos. Load 12 per page. Click to load more images.

--- viewer/photos/urls.py
+++ viewer/photos/urls.py
@@ -1,11 +1,12 @@
 # viewer/photos/urls.py
 
 from django.urls import path
-from photos.views import dashboard, detail
+from photos.views import dashboard, page, detail
 
 app_name = "photos"
 
 urlpatterns = [
     path("", dashboard, name="dashboard"), # <--- Dashboard View
+    path("page/<int:page_num>", page, name="page"), # <--- Page View
     path("<int:pk>/", detail, name="detail"), # <--- Detail View
 ]
--- viewer/photos/views.py
+++ viewer/photos/views.py
@@ -5,6 +5,7 @@ import logging
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.utils import timezone
+from django.core.paginator import Paginator, Page
 from django.views.decorators.http import require_POST
 
 from photos.models import Photo
@@ -14,19 +15,62 @@ logger = logging.getLogger(__name__)
 
 
 def dashboard(request: HttpRequest) -> HttpResponse:
-    display = "list"
     photos = Photo.objects.all().filter(
         pub_date__lte=timezone.now()
     ).order_by('-pub_date')
+
+    paginator = Paginator(photos, 12)
+    page = paginator.page(1)
+
     return render(
         request,
         "photos/dashboard.html",
         {
-            "display": display,
-            "photos": photos
+            "page": page
         }
     )
 
 
+@require_POST
+def page(request: HttpRequest, page_num: int) -> HttpResponse:
+    photos = Photo.objects.all().filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')
+
+    paginator = Paginator(photos, 12)
+    current_page = paginator.page(page_num)
+
+    hide_button = False
+
+    if current_page.has_next():
+        next_page = paginator.page(current_page.next_page_number())
+        if not next_page.has_next():
+            hide_button = True
+        return render(
+            request,
+            "photos/photo_list.html",
+            {
+                "page": next_page,
+                "hide_button": hide_button
+            }
+        )
+    else:
+        hide_button = True
+        return render(
+            request,
+            "photos/photo_list.html",
+            {
+                "hide_button": hide_button
+            }
+        )
+
+
 def detail(request: HttpResponse, pk: int) -> HttpResponse:
-    pass
+    photo = Photo.objects.get(id=pk)
+    return render(
+        request,
+        "photos/detail.html",
+        {
+            "photo": photo
+        }
+    )
