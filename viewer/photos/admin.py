97d9c1a1f570c3e7d1f0b243e4e142c4f55714d0
Neil <nhc.crum@gmail.com>
2023-03-12 00:22:57 -0700

    Register Photo to admin site

--- viewer/photos/admin.py
+++ viewer/photos/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from photos.models import Photo
 
-# Register your models here.
+class PhotoAdmin(admin.ModelAdmin):
+    model = Photo
+
+    list_display = ("name", "pub_date")
+    list_filter = ("name", "pub_date")
+
+    fieldsets = (
+        (None, {
+            "fields": (
+                "name",
+                "image",
+                "description",
+                "taken_date"
+            ),
+        }),
+    )
+    
+    search_fields = ("name", "pub_date")
+
+admin.site.register(Photo, PhotoAdmin)
